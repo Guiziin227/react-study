958af8f2da86adf66bd82623f603f0f7bab0d6c8
Guilherme Weber <gh63388@gmail.com>
2024-10-14 14:41:29 -0300

    add loading details

--- 4-Letterboxdv2/letter-boxd/src/App.js
+++ 4-Letterboxdv2/letter-boxd/src/App.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import "./index.css"
+import StarRating from "./StarRating";
 
 const tempMovieData = [
     {
@@ -62,7 +63,7 @@ export default function App() {
     const [selectedId, setSelectedId] = useState(null)
 
     function handleSelectedId(id) {
-        setSelectedId(selectedId => (selectedId === id ? id : null));
+        setSelectedId(id);
     }
 
     function handleCloseId() {
@@ -175,8 +176,60 @@ function Movies({movie, onSelectMovie}) {
 }
 
 function SelectedMovie({selectedId, onCloseId}) {
+
+    const [movie, setMovie] = useState({})
+    const [loading, setLoading] = useState(false)
+    const {
+        Title: title,
+        Year: year,
+        Poster: poster,
+        Runtime: runtime,
+        imdbRating,
+        Plot: plot,
+        Released: released,
+        Actors: actors,
+        Director: director,
+        Genre: genre,
+    } = movie
+
+    console.log(title, year)
+
+    useEffect(() => {
+        setLoading(true)
+
+        async function getDetailsMovie() {
+            const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`)
+            const data = await res.json()
+            setMovie(data)
+            setLoading(false)
+        }
+
+        getDetailsMovie()
+
+    }, [selectedId])
+
+
     return <div className="details">
-        <button className="btn-back" onClick={onCloseId}>X</button>
+        {loading ? <Loader/> : <>
+            <header>
+                <button className="btn-back" onClick={onCloseId}>X</button>
+                <img src={poster} alt={title}/>
+                <div className="details-overview">
+                    <h2>{title}</h2>
+                    <p>{released} &bull; {runtime}</p>
+                    <p>{genre}</p>
+                    <p><span>✨</span>{imdbRating} IMDb rating</p>
+                </div>
+            </header>
+
+            <section>
+                <div className="rating"><StarRating maxStars={10} size={26}/></div>
+                <p><em>{plot}</em></p>
+                <p>Atores: {actors}</p>
+                <p>Diretor: {director}</p>
+            </section>
+        </>
+        }
         {selectedId}</div>
 }
 
