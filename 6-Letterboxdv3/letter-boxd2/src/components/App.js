3fb5bcee83f9013e352d037a9a8769b2ef07bcca
Guilherme Weber <gh63388@gmail.com>
2024-10-20 17:24:33 -0300

    Add focus in search w/ useRef

--- 6-Letterboxdv3/letter-boxd2/src/components/App.js
+++ 6-Letterboxdv3/letter-boxd2/src/components/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import "../index.css"
 import StarRating from "../StarRating";
 
@@ -348,11 +348,32 @@ function NumResults({movies}) {
 }
 
 function Search({query, setQuery}) {
+    const inputEl = useRef(null);
+
     useEffect(() => {
-        const el = document.querySelector('.search');
-        console.log(el)
-        el.focus()
-    }, []);
+
+        function callback(e) {
+            if (document.activeElement === inputEl.current) return
+
+            if (e.code === "Enter") {
+                inputEl.current.focus();
+                setQuery("");
+            }
+        }
+
+        document.addEventListener("keydown", callback);
+
+        return () => {
+            document.removeEventListener("keydown", callback);
+        }
+
+    }, [setQuery])
+
+    // useEffect(() => {
+    //     const el = document.querySelector('.search');
+    //     console.log(el)
+    //     el.focus()
+    // }, []);
 
     return <input
         className="search"
@@ -360,5 +381,6 @@ function Search({query, setQuery}) {
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
     />
 }
