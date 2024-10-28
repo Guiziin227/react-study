8e63c8bbe18a5b2f85dec578781c190d960e7163
Guilherme Weber <gh63388@gmail.com>
2024-10-28 19:31:49 -0300

    use Reducer 2 conditions

--- 7-quiz/quiz/src/App.js
+++ 7-quiz/quiz/src/App.js
@@ -1,14 +1,39 @@
 import Header from "./Header";
 import Main from "./Main";
-import {useEffect} from "react";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+    quetions: [],
+    //loading, error, ready, active, finished
+    status: "loading",
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "dataReceived":
+            return {...state, quetions: action.payload, status: "ready"};
+        case "dataFailed":
+            return {...state, status: "error"};
+        default:
+            return state;
+    }
+}
 
 export default function App() {
 
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     useEffect(() => {
         async function fetchData() {
-            const res = await fetch("http://localhost:8000/questions");
-            const data = await res.json();
-            console.log(data);
+            try {
+                const res = await fetch("http://localhost:8000/questions");
+                const data = await res.json();
+                dispatch({type: "dataReceived", payload: data});
+                console.log(data);
+            } catch (error) {
+                dispatch({type: "dataFailed"})
+            }
+
         }
 
         fetchData(); // Chama a função renomeada
