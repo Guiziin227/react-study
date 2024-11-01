0573dc45b94594e759fd53bda2a881f491baa96b
Guilherme Weber <gh63388@gmail.com>
2024-11-01 16:56:13 -0300

    add css module

--- 8-SPA/WorldWise/src/components/NavBar.jsx
+++ 8-SPA/WorldWise/src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import {NavLink} from "react-router-dom";
+import styles from "./NavBar.module.css"
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 <li>
                     <NavLink to="/">Home</NavLink>
