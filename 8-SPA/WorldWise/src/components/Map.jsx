609f8ea2f46fa6c0eda1914edbfb2c9d256c1fdd
Guilherme Weber <gh63388@gmail.com>
2024-11-20 12:08:26 -0300

    adicionando os pins no mapa

--- 8-SPA/WorldWise/src/components/Map.jsx
+++ 8-SPA/WorldWise/src/components/Map.jsx
@@ -2,11 +2,14 @@ import style from "./Map.module.css"
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {useState} from "react";
+import {useCities} from "../contexts/CitiesContext.jsx";
 
 const Map = () => {
 
     const navigate = useNavigate()
     const [mapPosition, setMapPosition] = useState([40, 0]);
+
+    const {cities} = useCities();
 
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get("lat")
@@ -14,15 +17,17 @@ const Map = () => {
 
     return (
         <div className={style.mapContainer}>
-            <MapContainer center={mapPosition} zoom={13} scrollWhellZoom={false} className={style.map}>
+            <MapContainer center={mapPosition} zoom={13} scrollWhellZoom={true} className={style.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                <Marker position={mapPosition}>
-                    <Popup>
-                        Ola
-                    </Popup>
-                </Marker>
+                {cities.map((city) => (
+                    <Marker key={city.id} position={[city.position.lat, city.position.lng]}>
+                        <Popup>
+                            <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
 
 
             </MapContainer>
